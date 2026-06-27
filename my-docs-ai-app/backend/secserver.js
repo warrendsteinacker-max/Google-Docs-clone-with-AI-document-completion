ba7b191548477cc2f3aca7d1b769d133a7f9d7cd
Warren <warrendsteinacker@gmail.com>
2026-06-27 18:33:05 -0400

    doing some more prac

--- my-docs-ai-app/backend/secserver.js
+++ my-docs-ai-app/backend/secserver.js
@@ -3,58 +3,111 @@
 
 
 
-const quary = [
+// const quary = [
 
-    {
-        name: "apple",
-        count: 3
-    },
-    {
-        name: "banana",
-        count: 5
+//     {
+//         name: "apple",
+//         count: 3
+//     },
+//     {
+//         name: "banana",
+//         count: 5
+//     }
+// ]
+
+// const searchterm = "a"
+
+
+
+// function searchf(quary, searchterm, ...rest){
+
+//     if(!quary || !searchterm){
+//         throw new Error("need quary and searchterm");
+//     }
+
+//     if(!Array.isArray(quary) || typeof searchterm !== "string"){
+//         throw new Error("need quary to be array and searchterm to be string");
+//     }
+
+//     if(rest){
+
+//         let pquary 
+
+//         for(let i = 0, len = rest.length; i < len; i++){
+//             pquary = quary[rest[i]];
+//             console.log(pquary);
+//         }
+
+//         const search = pquary.filter((item) => {
+//         return item.toLowerCase().includes(searchterm.toLowerCase());
+//     })
+
+//     return search;
+
+//     }
+
+//     const search = quary.filter((item) => {
+//         return item.toLowerCase().includes(searchterm.toLowerCase());
+//     })
+
+//     return search;
+// }
+
+
+// console.log(searchf(quary, searchterm, "name"))
+
+
+
+
+const dec = "1.03";
+
+function makeDtoSN(dec) {
+
+    const newDec = String(dec)
+    console.log(newDec[0])
+
+    if(newDec[0] === "0"){
+        const secnewDec = newDec.split();
+        console.log(secnewDec)
+        const indexdec = secnewDec.indexOf(".");
+        console.log()
+        const firstdigI = secnewDec.forEach((item, index) => {
+            if(Number(item) !== 0){
+                return index;
+            }
+        })[0]
+
+        const power = String(firstdigI - indexdec)
+
+        const partofdec = secnewDec.slice(firstdigI, secnewDec.length);
+        console.log(partofdec)
+        const newpartofD = partofdec.splice(0, 0, ".")
+        console.log(newpartofD)
+
+        return `${newpartofD.join("")}^-${power}`;
     }
-]
-
-const searchterm = "a"
-
-
-
-function searchf(quary, searchterm, ...rest){
-
-    if(!quary || !searchterm){
-        throw new Error("need quary and searchterm");
-    }
-
-    if(!Array.isArray(quary) || typeof searchterm !== "string"){
-        throw new Error("need quary to be array and searchterm to be string");
-    }
-
-    if(rest){
-
-        let pquary 
-
-        for(let i = 0, len = rest.length; i < len; i++){
-            pquary = quary[rest[i]];
-            console.log(pquary);
+    else{
+        console.log("hello")
+        const secnewDec = newDec.split();
+        const indexdec = secnewDec.indexOf(".");
+        if(indexdec === 1){
+            return newDec.join("") + "^" + String(secnewDec.length - 1);
         }
-
-        const search = pquary.filter((item) => {
-        return item.toLowerCase().includes(searchterm.toLowerCase());
-    })
-
-    return search;
-
+        const power =  secnewDec.slice(0, indexdec).length;
+        console.log(power)
+        const partofdec = secnewDec.splice(indexdec, 1)
+        const newpartofD = partofdec.splice(0, 0, ".")
+         return `${newpartofD.join("")}^${power}`;
     }
 
-    const search = quary.filter((item) => {
-        return item.toLowerCase().includes(searchterm.toLowerCase());
-    })
 
-    return search;
+
 }
 
 
-console.log(searchf(quary, searchterm, "name"))
+
+console.log(makeDtoSN(dec))
+
 
 
 
