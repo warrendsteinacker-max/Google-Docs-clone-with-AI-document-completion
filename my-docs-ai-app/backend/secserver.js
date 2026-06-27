e7d9c6340099fd76a4bf98d362d723c40df60408
Warren <warrendsteinacker@gmail.com>
2026-06-27 18:58:05 -0400

    doing some more prac

--- my-docs-ai-app/backend/secserver.js
+++ my-docs-ai-app/backend/secserver.js
@@ -59,54 +59,54 @@
 
 
 
-const dec = "1.03";
+// const dec = "1.03";
 
-function makeDtoSN(dec) {
+// function makeDtoSN(dec) {
 
-    const newDec = String(dec)
-    console.log(newDec[0])
+//     const newDec = String(dec)
+//     console.log(newDec[0])
 
-    if(newDec[0] === "0"){
-        const secnewDec = newDec.split();
-        console.log(secnewDec)
-        const indexdec = secnewDec.indexOf(".");
-        console.log()
-        const firstdigI = secnewDec.forEach((item, index) => {
-            if(Number(item) !== 0){
-                return index;
-            }
-        })[0]
+//     if(newDec[0] === "0"){
+//         const secnewDec = newDec.split();
+//         console.log(secnewDec)
+//         const indexdec = secnewDec.indexOf(".");
+//         console.log()
+//         const firstdigI = secnewDec.forEach((item, index) => {
+//             if(Number(item) !== 0){
+//                 return index;
+//             }
+//         })[0]
 
-        const power = String(firstdigI - indexdec)
+//         const power = String(firstdigI - indexdec)
 
-        const partofdec = secnewDec.slice(firstdigI, secnewDec.length);
-        console.log(partofdec)
-        const newpartofD = partofdec.splice(0, 0, ".")
-        console.log(newpartofD)
+//         const partofdec = secnewDec.slice(firstdigI, secnewDec.length);
+//         console.log(partofdec)
+//         const newpartofD = partofdec.splice(0, 0, ".")
+//         console.log(newpartofD)
 
-        return `${newpartofD.join("")}^-${power}`;
-    }
-    else{
-        console.log("hello")
-        const secnewDec = newDec.split();
-        const indexdec = secnewDec.indexOf(".");
-        if(indexdec === 1){
-            return newDec.join("") + "^" + String(secnewDec.length - 1);
-        }
-        const power =  secnewDec.slice(0, indexdec).length;
-        console.log(power)
-        const partofdec = secnewDec.splice(indexdec, 1)
-        const newpartofD = partofdec.splice(0, 0, ".")
-         return `${newpartofD.join("")}^${power}`;
-    }
-
-
-
-}
+//         return `${newpartofD.join("")}^-${power}`;
+//     }
+//     else{
+//         console.log("hello")
+//         const secnewDec = newDec.split();
+//         const indexdec = secnewDec.indexOf(".");
+//         if(indexdec === 1){
+//             return newDec.join("") + "^" + String(secnewDec.length - 1);
+//         }
+//         const power =  secnewDec.slice(0, indexdec).length;
+//         console.log(power)
+//         const partofdec = secnewDec.splice(indexdec, 1)
+//         const newpartofD = partofdec.splice(0, 0, ".")
+//          return `${newpartofD.join("")}^${power}`;
+//     }
 
 
 
-console.log(makeDtoSN(dec))
+// }
+
+
+
+// console.log(makeDtoSN(dec))
 
 
 
